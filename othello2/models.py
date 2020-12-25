050f97c7fa61e972cc48d24dd3dc99bc458c7ffa
rentalMac <>
2020-12-25 11:31:49 +0900

    PCデータのデータ構造

--- othello2/models.py
+++ othello2/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class PlayerCharacters(models.Model):
+    borad_scores = models.CharField('カンマ区切りスコア', max_length=50, unique=True, db_index=True)
+    score01 = models.SmallIntegerField()
+    score02 = models.SmallIntegerField()
+    score03 = models.SmallIntegerField()
+    score04 = models.SmallIntegerField()
+    score05 = models.SmallIntegerField()
+    score06 = models.SmallIntegerField()
+    score07 = models.SmallIntegerField()
+    score08 = models.SmallIntegerField()
+    score09 = models.SmallIntegerField()
+    score10 = models.SmallIntegerField()
+    borad_score_dict = models.TextField('スコアインデックスを計算したJSON')
+    weight1 = models.SmallIntegerField()
+    weight2 = models.SmallIntegerField()
+    weight3 = models.SmallIntegerField()
+    match_count = models.IntegerField('試合数', default=0)
+    win_count = models.IntegerField('勝ち数', default=0)
+    lose_count = models.IntegerField('負け数', default=0)
+    total_stone = models.IntegerField('試合終了時の石の数の合計', default=0)
