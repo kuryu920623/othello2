111627b8262699bae13d2b1daf26a8067de5110a
rentalMac <>
2021-03-13 10:06:15 +0900

    本番でキャッシュ

--- config/settings_prd.py
+++ config/settings_prd.py
@@ -4,3 +4,10 @@ DEBUG = False
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.db.DatabaseCache',
+        'LOCATION': 'my_cache_table',
+    }
+}
